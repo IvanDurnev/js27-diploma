dbab02554c23b616a856576ee71c84be613e911e
Ivan V. Durnev <durnev87@gmail.com>
2019-03-25 16:26:59 +0900

    Vector realised

--- game.js
+++ game.js
@@ -1,1 +1,27 @@
 'use strict';
+
+class Vector {
+	constructor(x = 0, y = 0) {
+		this.x = x;
+		this.y = y;
+	};
+
+	plus(vector) {
+		if (vector instanceof Vector) {
+      return new Vector(this.x + vector.x, this.y + vector.y);
+    } else {
+      throw 'Можно прибавлять к вектору только вектор типа Vector';
+    }
+	};
+
+  times(number) {
+    return new Vector(this.x * number, this.y * number);
+  };
+}
+
+const start = new Vector(30, 50);
+const moveTo = new Vector(5, 10);
+const finish = start.plus(moveTo.times(2));
+
+console.log(`Исходное расположение: ${start.x}:${start.y}`);
+console.log(`Текущее расположение: ${finish.x}:${finish.y}`);
