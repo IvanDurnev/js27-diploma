d3eee7939547f9396076a4b1a37f51579fcbd7ca
Ivan V. Durnev <durnev87@gmail.com>
2019-03-26 08:59:34 +0900

    Actor realised

--- game.js
+++ game.js
@@ -8,20 +8,65 @@ class Vector {
 
 	plus(vector) {
 		if (vector instanceof Vector) {
-			return new Vector(this.x + vector.x, this.y + vector.y);
-		} else {
-			throw 'Можно прибавлять к вектору только вектор типа Vector';
-		};
+      return new Vector(this.x + vector.x, this.y + vector.y);
+    } else {
+      throw 'Можно прибавлять к вектору только вектор типа Vector';
+    }
 	};
 
-  	times(number) {
-  		return new Vector(this.x * number, this.y * number);
-  	};
-}
+  times(number) {
+    return new Vector(this.x * number, this.y * number);
+  };
+};
 
-const start = new Vector(30, 50);
-const moveTo = new Vector(5, 10);
-const finish = start.plus(moveTo.times(2));
+class Actor {
+  constructor(pos = new Vector(0,0), size = new Vector(1,1), speed = new Vector(0,0)) {
+    this.pos = pos;
+    this.size = size;
+    this.speed = speed;
+    Object.defineProperties(this, {
+      'left': {
+        get: function(){
+          return this.pos.x
+        }
+      },
+      'top': {
+        get: function(){
+          return this.pos.y
+        }
+      },
+      'right': {
+        get: function(){
+          return this.pos.x + this.size.x
+        }
+      },
+      'bottom': {
+        get: function(){
+          return this.pos.y + this.size.y
+        }
+      },
+      'type': {
+        value: 'actor',
+        writable: false
+      }
+    });
+    
+  };
 
-console.log(`Исходное расположение: ${start.x}:${start.y}`);
-console.log(`Текущее расположение: ${finish.x}:${finish.y}`);
+  act() {};
+
+  isIntersect(actor) {
+    if (actor instanceof Actor) {
+      if (actor === this) {
+        return false;
+      } else {
+        if ((this.left >= actor.left && this.left < actor.right) || (this.right > actor.left && this.right <= actor.right)
+          && (this.top >= actor.top && this.top < actor.bottom) || (this.bottom > actor.top && this.bottom <= actor.bottom)) {
+          return true;
+        }
+      }
+    } else {
+      throw 'Передан не объект Actor';
+    }
+  };
+};
