c4298ad90c8c3704203da01fa49fe35ffe3bb27e
Ivan V. Durnev <durnev87@gmail.com>
2019-03-26 09:50:19 +0900

    Vector and Actor realised and checked

--- game.js
+++ game.js
@@ -10,7 +10,7 @@ class Vector {
 		if (vector instanceof Vector) {
 			return new Vector(this.x + vector.x, this.y + vector.y);
 		} else {
-			throw 'Можно прибавлять к вектору только вектор типа Vector';
+			throw Error('Можно прибавлять к вектору только вектор типа Vector');
 		}
 	};
 	
@@ -21,52 +21,70 @@ class Vector {
 
 class Actor {
 	constructor(pos = new Vector(0,0), size = new Vector(1,1), speed = new Vector(0,0)) {
-		this.pos = pos;
-		this.size = size;
-    	this.speed = speed;
-    	Object.defineProperties(this, {
-      		'left': {
-        		get: function(){
-					return this.pos.x
+		if ((pos instanceof Vector) && (size instanceof Vector) && (speed instanceof Vector)) {
+			this.pos = pos;
+			this.size = size;
+    		this.speed = speed;
+    		Object.defineProperties(this, {
+      			'left': {
+        			get: function(){
+						return this.pos.x
+					}
+				},
+				'top': {
+					get: function(){
+					return this.pos.y
+					}
+				},
+				'right': {
+					get: function(){
+					return this.pos.x + this.size.x
+					}
+				},
+				'bottom': {
+					get: function(){
+					return this.pos.y + this.size.y
+					}
+				},
+				'type': {
+					value: 'actor',
+					writable: false
 				}
-			},
-      		'top': {
-        		get: function(){
-          		return this.pos.y
-        		}
-			},
-      		'right': {
-        		get: function(){
-          		return this.pos.x + this.size.x
-        		}
-      		},
-      		'bottom': {
-        		get: function(){
-          		return this.pos.y + this.size.y
-        		}
-      		},
-      		'type': {
-        		value: 'actor',
-        		writable: false
-      		}
-    	});
-    
+			});
+		} else {
+			throw Error();
+		}
   	};
 
 	act() {};
 
-	isIntersect(actor) {
-    	if (actor instanceof Actor) {
-      		if (actor === this) {
+	isIntersect(other) {
+    	if (other instanceof Actor) {
+      		if (other === this) {
         		return false;
 			} else {
-        		if ((this.left >= actor.left && this.left < actor.right) || (this.right > actor.left && this.right <= actor.right)
-          			&& (this.top >= actor.top && this.top < actor.bottom) || (this.bottom > actor.top && this.bottom <= actor.bottom)) {
+				if (
+                (
+					(this.left > other.left && this.left < other.right) || (this.right > other.left && this.right < other.right)
+					&& 
+					(this.top > other.top && this.top < other.bottom) || (this.bottom > other.top && this.bottom < other.bottom)
+				)
+					||
+				(
+					(other.left > this.left && other.left < this.right) || (other.right > this.left && other.right < this.right)
+					&& 
+					(other.top > this.top && other.top < this.bottom) || (other.bottom > this.top && other.bottom < this.bottom)
+				)
+					&&
+					((this.right !== other.left) && (this.bottom !== other.top) && (this.left !== other.right) && (this.top !== other.bottom)) 
+				) {
           			return true;
-        			}
+				} else {
+					return false;
+				}
 			}
 		} else {
-      			throw 'Передан не объект Actor';
+      			throw Error('Передан не объект Actor');
 			}
 	};
 };
